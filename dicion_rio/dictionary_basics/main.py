535a5a57269a856d0ad45c5485fd11d003c0f1da
TilleyBeatz <>
2026-05-27 06:30:10 +0000

    Dictionary Basics

---
+++ dicion_rio/dictionary_basics/main.py
@@ -0,0 +1,9 @@
+# Write your code here
+library = {
+    "title 1": "The Great Gatsby|F.Scott Fitzgerald",
+    "Title 2":"1985|George Orwell",
+    "title 3":"To Kill a Mockingbird | Harper Lee"
+}
+
+# Testing
+print('Library dictionary:', library)
